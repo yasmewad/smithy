ebe0e2d67b1da35e55a2606c10ce7d5d96f84944
Yash Mewada <yasmewad@amazon.com>
2024-11-08 16:05:33 -0800

    Converting existing gradle DSL from groovy to kotlin.
    ** Why is this needed ? **
    This is the first step in moving to a more consistent and composable experience for gradle configuration in the smithy repository. Writing it in kotlin to make it more expressive as compared to groovy. This commit just converts the existing gradle build from groovy to kotlin.
    
    ** Next steps: **
    Once this change is merged, few other PRs will follow to introduce nice features such as `buildSrc` plugins to enhance configurability of how each sub project builds in this package. It will introduce flexibility in things such as allowing each subproject to compile with different JVM toolchain, and much more.

---
+++ settings.gradle.kts
@@ -0,0 +1,46 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "smithy"
+
+val subProjects = listOf(
+    "smithy-aws-iam-traits",
+    "smithy-aws-traits",
+    "smithy-aws-apigateway-traits",
+    "smithy-aws-apigateway-openapi",
+    "smithy-aws-protocol-tests",
+    "smithy-cli",
+    "smithy-codegen-core",
+    "smithy-build",
+    "smithy-model",
+    "smithy-diff",
+    "smithy-linters",
+    "smithy-mqtt-traits",
+    "smithy-jsonschema",
+    "smithy-openapi",
+    "smithy-openapi-traits",
+    "smithy-utils",
+    "smithy-protocol-test-traits",
+    "smithy-jmespath",
+    "smithy-waiters",
+    "smithy-aws-cloudformation-traits",
+    "smithy-aws-cloudformation",
+    "smithy-validation-model",
+    "smithy-rules-engine",
+    "smithy-smoke-test-traits",
+    "smithy-syntax",
+    "smithy-aws-endpoints",
+    "smithy-aws-smoke-test-model",
+    "smithy-protocol-traits",
+    "smithy-protocol-tests",
+    "smithy-trait-codegen"
+)
+
+subProjects.forEach {
+    include(":$it")
+}
